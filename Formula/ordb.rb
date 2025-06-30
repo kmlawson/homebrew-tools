cd1772fd54f5e74eec9fad8164d5cbe6e21e9f85
Konrad Lawson <kl@muninn.net>
2025-06-30 13:56:02 +0200

    ordb: update to version 0.5.3
    - Update URL to PyPI package for v0.5.3
    - Update SHA256 hash for new package
    - Switch to python@3.11 for better compatibility

--- Formula/ordb.rb
+++ Formula/ordb.rb
@@ -3,11 +3,11 @@ class Ordb < Formula
 
   desc "Fast, feature-rich command-line tool for searching Norwegian bokmål dictionary"
   homepage "https://github.com/kmlawson/ordb"
-  url "https://files.pythonhosted.org/packages/f8/0f/70fccaf0a559ee299ce1026c2a22f7de089a9d2060a2799a22cdd9e6df1d/ordb-0.5.0.tar.gz"
-  sha256 "175136300f286ba9badc9807046b057fb9f91d9b4ab2356e90c6857dd4c2efd9"
+  url "https://files.pythonhosted.org/packages/e4/f4/bbcaf604b2ca886c4dd30a487bf197411c51efed30d2d018f786270268f4/ordb-0.5.3.tar.gz"
+  sha256 "b247c308d06cf2d3f3cf55dde7ba40565915384c96e1e9077c13afceade3689a"
   license "MIT"
 
-  depends_on "python@3.13"
+  depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
